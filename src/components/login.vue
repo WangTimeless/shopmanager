7ac8fd50462a0215041d67f163ba10acd149602e
wwp <782568523@qq.com>
2019-01-02 20:14:12 +0800

    渲染home组件

---
+++ src/components/login.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="wrap">
+    <el-form class="form" label-position="top" label-width="80px" :model="formData">
+        <h2>用户登录</h2>
+      <el-form-item label="用户名">
+        <el-input v-model="formData.username"></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input v-model="formData.password"></el-input>
+      </el-form-item>
+      <el-button @click="handleLogin()" type="primary" class="btn">登录</el-button>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      formData: {
+        username: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    handleLogin () {
+      this.$http
+        .post('login', this.formData)
+        .then((res) => {
+        //   console.log(res)
+          const {data: {meta: {msg, status}}} = res
+          if (status === 200) {
+            this.$router.push({
+               name: "home"
+            });
+          } else {
+            this.$message.warning(msg);
+          }
+        })
+    }
+  }
+}
+</script>
+
+<style>
+.wrap {
+    height: 100%;
+    background-color: #324152;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.wrap .form{
+    background-color: #fff;
+    border-radius: 10px;
+    width: 400px;
+    padding: 30px;
+}
+.wrap .form .btn {
+    width: 100%;
+}
+</style>
